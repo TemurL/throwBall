bb614c81595ed5ec3f3fa70a8a8fcb979a1fc923
Temur Lakerbaya <lakerbaya00@mail.ru>
2025-03-28 03:37:34 +0300

    disable touck select

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -50,7 +50,7 @@ const gunMouseup = () => {
 <template>
     <h2 class="page-title">Game</h2>
 
-    <div id="throwBall" :style="`height:${gameState.global.boardHeight}px`">
+    <div id="throwBall" :style="`height:${gameState.global.boardHeight}px; user-select: none;`">
         <span>Level: {{ gameState.levelProps.index }}</span>
         <div class="lives">
             <svg width="62" height="61" viewBox="0 0 62 61" fill="none" xmlns="http://www.w3.org/2000/svg">
