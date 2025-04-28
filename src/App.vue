50119fd5808311cb93ba329c103b735111249bb4
Temur Lakerbaya <lakerbaya00@mail.ru>
2025-04-28 21:26:19 +0300

    vercel analytics fix2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
-import { Analytics } from '@vercel/analytics';
-
 </script>
 
 <template>
   <Header/>
   <RouterView />
-  <Analytics />
 </template>
 
 <style scoped>
