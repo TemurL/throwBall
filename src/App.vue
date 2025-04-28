32e3c7a30ca851059d608200d3d831b214ce0a9d
Temur Lakerbaya <lakerbaya00@mail.ru>
2025-04-29 02:56:12 +0300

    add vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
 import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
   <Analytics />
   <Header/>
   <RouterView />
+  <SpeedInsights />
 </template>
 
 <style scoped>
