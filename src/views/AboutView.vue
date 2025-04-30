d3d2b0c89d19feafb7753d6e8ed575f1c81eea56
Temur Lakerbaya <lakerbaya00@mail.ru>
2025-04-30 05:57:39 +0300

    edit about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h2 class="page-title">Throw ball game</h2>
     <ul>
-      <li>Hold Space/click on a gun to load (no touchscreen)</li>
+      <li>Hold Space to load the gun</li>
       <li>Release to shoot</li>
       <li>Hit the target height</li>
     </ul>
